f1476ea145921e3d190593666f8e701e008d17ad
sineptic <sineptic0@gmail.com>
2026-04-08 19:06:06 +0300

    refactor main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 const EXAMPLE_TEXT: &str = r#"
 13 task - `hi`
 Algorithm:
@@ -26,163 +28,240 @@ fn main() {
 
     let mut cursor = 0;
     loop {
-        crossterm::execute!(
-            stdout,
-            crossterm::terminal::Clear(crossterm::terminal::ClearType::All),
-            crossterm::cursor::MoveTo(0, 0)
-        )
-        .unwrap();
-        print!("\x1b[0m");
-        let mut interactive_element_in_focus = false;
-        for (i, block) in blocks.iter().enumerate() {
-            match block {
-                DisplayBlock::Text(inner) => {
-                    print!("{}", inner.replace("\n", "\r\n"));
-                }
-                DisplayBlock::HiddenText {
-                    original_text: _,
-                    user_input,
-                    field_cursor,
-                } => {
-                    if user_input.is_empty() {
-                        print!("\x1b[1;4;34m ");
-                        if i == cursor {
-                            interactive_element_in_focus = true;
-                            crossterm::execute!(stdout, crossterm::cursor::SavePosition).unwrap();
-                        }
-                        print!("  \x1b[0m");
-                    } else {
-                        print!(
-                            "\x1b[1;3;4m{}",
-                            user_input.iter().take(*field_cursor).collect::<String>()
-                        );
-                        if i == cursor {
-                            interactive_element_in_focus = true;
-                            crossterm::execute!(stdout, crossterm::cursor::SavePosition).unwrap();
-                        }
-                        print!(
-                            "{}",
-                            user_input.iter().skip(*field_cursor).collect::<String>()
-                        );
-                        // print!("\x1b[0;1;34m|");
-                        print!("\x1b[0m");
-                    }
-                }
-            }
+        display_blocks_interactive_mode(&mut stdout, &blocks, cursor);
+        let event = crossterm::event::read().unwrap();
+        if handle_event(event, &mut cursor, &mut blocks) {
+            break;
         }
-        println!();
-        if interactive_element_in_focus {
-            crossterm::execute!(
-                stdout,
-                crossterm::cursor::RestorePosition,
-                crossterm::cursor::Show
-            )
-            .unwrap();
-        } else {
-            crossterm::execute!(stdout, crossterm::cursor::Hide).unwrap();
-        }
-        match crossterm::event::read().unwrap() {
-            crossterm::event::Event::Key(key_event) => {
-                if key_event.is_press() {
-                    match key_event.code {
-                        crossterm::event::KeyCode::Char(ch) => {
-                            match &mut blocks[cursor] {
-                                DisplayBlock::Text(_) => {
-                                    // there could be no interactive elements
-                                }
-                                DisplayBlock::HiddenText {
-                                    original_text: _,
-                                    user_input,
-                                    field_cursor,
-                                } => {
-                                    user_input.insert(*field_cursor, ch);
-                                    *field_cursor += 1;
-                                }
-                            };
-                        }
-                        crossterm::event::KeyCode::Backspace => {
-                            match &mut blocks[cursor] {
-                                DisplayBlock::Text(_) => {
-                                    // there could be no interactive elements
-                                }
-                                DisplayBlock::HiddenText {
-                                    original_text: _,
-                                    user_input,
-                                    field_cursor,
-                                } => {
-                                    if *field_cursor != 0 {
-                                        user_input.remove(*field_cursor - 1);
-                                        *field_cursor -= 1;
-                                    }
-                                }
-                            };
-                        }
-                        crossterm::event::KeyCode::Delete => {
-                            match &mut blocks[cursor] {
-                                DisplayBlock::Text(_) => {
-                                    // there could be no interactive elements
-                                }
-                                DisplayBlock::HiddenText {
-                                    original_text: _,
-                                    user_input,
-                                    field_cursor,
-                                } => {
-                                    if *field_cursor < user_input.len() {
-                                        user_input.remove(*field_cursor);
-                                    }
-                                }
-                            };
-                        }
-                        crossterm::event::KeyCode::Enter => todo!(),
-                        crossterm::event::KeyCode::Left => {
-                            *blocks[cursor].field_cursor() =
-                                blocks[cursor].field_cursor().saturating_sub(1);
-                        }
-                        crossterm::event::KeyCode::Right => {
-                            let user_input_len = blocks[cursor].as_hidden_user_text().len();
-                            if *blocks[cursor].field_cursor() < user_input_len {
-                                *blocks[cursor].field_cursor() += 1;
-                            }
-                        }
-                        crossterm::event::KeyCode::Tab | crossterm::event::KeyCode::Down => {
-                            let mut new_cursor = cursor + 1;
-                            loop {
-                                if new_cursor >= blocks.len() {
-                                    break;
-                                }
-                                if matches!(blocks[new_cursor], DisplayBlock::HiddenText { .. }) {
-                                    cursor = new_cursor;
-                                    break;
-                                }
-                                new_cursor += 1;
-                            }
-                        }
-                        crossterm::event::KeyCode::BackTab | crossterm::event::KeyCode::Up => {
-                            if cursor == 0 {
-                                continue;
-                            }
-                            let mut new_cursor = cursor - 1;
-                            loop {
-                                if matches!(blocks[new_cursor], DisplayBlock::HiddenText { .. }) {
-                                    cursor = new_cursor;
-                                    break;
-                                }
-                                if new_cursor == 0 {
-                                    break;
-                                }
-                                new_cursor -= 1;
-                            }
-                        }
-                        crossterm::event::KeyCode::Esc => break,
-                        _ => {}
-                    }
-                }
+        // handle all available events
+        while crossterm::event::poll(Duration::ZERO).unwrap() {
+            let event = crossterm::event::read().unwrap();
+            if handle_event(event, &mut cursor, &mut blocks) {
+                break;
             }
-            _ => {}
         }
     }
+    display_blocks_answer_overview(&mut stdout, &blocks);
+    std::io::read_to_string(std::io::stdin()).unwrap();
     crossterm::execute!(stdout, crossterm::terminal::LeaveAlternateScreen,).unwrap();
     crossterm::terminal::disable_raw_mode().unwrap();
+}
+
+/// Returns `true` if should quit.
+fn handle_event(
+    event: crossterm::event::Event,
+    cursor: &mut usize,
+    blocks: &mut Vec<DisplayBlock>,
+) -> bool {
+    match event {
+        crossterm::event::Event::Key(key_event) => {
+            if key_event.is_press() {
+                match key_event.code {
+                    crossterm::event::KeyCode::Char(ch) => {
+                        match &mut blocks[*cursor] {
+                            DisplayBlock::Text(_) => {
+                                // there could be no interactive elements
+                            }
+                            DisplayBlock::HiddenText {
+                                original_text: _,
+                                user_input,
+                                field_cursor,
+                            } => {
+                                user_input.insert(*field_cursor, ch);
+                                *field_cursor += 1;
+                            }
+                        };
+                    }
+                    crossterm::event::KeyCode::Backspace => {
+                        match &mut blocks[*cursor] {
+                            DisplayBlock::Text(_) => {
+                                // there could be no interactive elements
+                            }
+                            DisplayBlock::HiddenText {
+                                original_text: _,
+                                user_input,
+                                field_cursor,
+                            } => {
+                                if *field_cursor != 0 {
+                                    user_input.remove(*field_cursor - 1);
+                                    *field_cursor -= 1;
+                                }
+                            }
+                        };
+                    }
+                    crossterm::event::KeyCode::Delete => {
+                        match &mut blocks[*cursor] {
+                            DisplayBlock::Text(_) => {
+                                // there could be no interactive elements
+                            }
+                            DisplayBlock::HiddenText {
+                                original_text: _,
+                                user_input,
+                                field_cursor,
+                            } => {
+                                if *field_cursor < user_input.len() {
+                                    user_input.remove(*field_cursor);
+                                }
+                            }
+                        };
+                    }
+                    crossterm::event::KeyCode::Enter => {
+                        let mut new_cursor = *cursor + 1;
+                        loop {
+                            if new_cursor >= blocks.len() {
+                                return true;
+                            }
+                            if matches!(blocks[new_cursor], DisplayBlock::HiddenText { .. }) {
+                                *cursor = new_cursor;
+                                break;
+                            }
+                            new_cursor += 1;
+                        }
+                    }
+                    crossterm::event::KeyCode::Left => {
+                        *blocks[*cursor].field_cursor() =
+                            blocks[*cursor].field_cursor().saturating_sub(1);
+                    }
+                    crossterm::event::KeyCode::Right => {
+                        let user_input_len = blocks[*cursor].as_hidden_user_text().len();
+                        if *blocks[*cursor].field_cursor() < user_input_len {
+                            *blocks[*cursor].field_cursor() += 1;
+                        }
+                    }
+                    crossterm::event::KeyCode::Tab | crossterm::event::KeyCode::Down => {
+                        let mut new_cursor = *cursor + 1;
+                        loop {
+                            if new_cursor >= blocks.len() {
+                                break;
+                            }
+                            if matches!(blocks[new_cursor], DisplayBlock::HiddenText { .. }) {
+                                *cursor = new_cursor;
+                                break;
+                            }
+                            new_cursor += 1;
+                        }
+                    }
+                    crossterm::event::KeyCode::BackTab | crossterm::event::KeyCode::Up => {
+                        if *cursor == 0 {
+                            return false;
+                        }
+                        let mut new_cursor = *cursor - 1;
+                        loop {
+                            if matches!(blocks[new_cursor], DisplayBlock::HiddenText { .. }) {
+                                *cursor = new_cursor;
+                                break;
+                            }
+                            if new_cursor == 0 {
+                                break;
+                            }
+                            new_cursor -= 1;
+                        }
+                    }
+                    crossterm::event::KeyCode::Esc => return true,
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
+    }
+    false
+}
+
+fn display_blocks_answer_overview(stdout: &mut std::io::Stdout, blocks: &Vec<DisplayBlock>) {
+    crossterm::execute!(
+        stdout,
+        crossterm::terminal::Clear(crossterm::terminal::ClearType::All),
+        crossterm::cursor::MoveTo(0, 0)
+    )
+    .unwrap();
+    print!("\x1b[0m");
+    for block in blocks {
+        match block {
+            DisplayBlock::Text(inner) => {
+                print!("{}", inner.replace("\n", "\r\n"));
+            }
+            DisplayBlock::HiddenText {
+                original_text,
+                user_input,
+                field_cursor: _,
+            } => {
+                let user_input = user_input.iter().collect::<String>();
+                let user_input = user_input.trim();
+                let original_text = original_text.trim();
+                if original_text.is_empty() {
+                    print!("\x1b[3;32m<empty>\x1b[0m");
+                } else {
+                    print!("\x1b[3;4;32m{}\x1b[0m", original_text);
+                }
+                if user_input != original_text {
+                    print!(" ");
+                    if user_input.is_empty() {
+                        print!("\x1b[3;33m<empty>\x1b[0m");
+                    } else {
+                        print!("\x1b[3;4;33m{}\x1b[0m", user_input);
+                    }
+                }
+            }
+        }
+    }
+    println!();
+    crossterm::execute!(stdout, crossterm::cursor::Hide).unwrap();
+}
+
+fn display_blocks_interactive_mode(
+    stdout: &mut std::io::Stdout,
+    blocks: &Vec<DisplayBlock>,
+    cursor: usize,
+) {
+    crossterm::execute!(
+        stdout,
+        crossterm::terminal::Clear(crossterm::terminal::ClearType::All),
+        crossterm::cursor::MoveTo(0, 0)
+    )
+    .unwrap();
+    print!("\x1b[0m");
+    let mut interactive_element_in_focus = false;
+    for (i, block) in blocks.iter().enumerate() {
+        match block {
+            DisplayBlock::Text(inner) => {
+                print!("{}", inner.replace("\n", "\r\n"));
+            }
+            DisplayBlock::HiddenText {
+                original_text: _,
+                user_input,
+                field_cursor,
+            } => {
+                if user_input.is_empty() && i != cursor {
+                    print!("\x1b[3m<empty>\x1b[0m");
+                } else {
+                    print!(
+                        "\x1b[3;4m{}",
+                        user_input.iter().take(*field_cursor).collect::<String>()
+                    );
+                    if i == cursor {
+                        interactive_element_in_focus = true;
+                        crossterm::execute!(stdout, crossterm::cursor::SavePosition).unwrap();
+                    }
+                    print!(
+                        "{}",
+                        user_input.iter().skip(*field_cursor).collect::<String>()
+                    );
+                    print!("\x1b[0m");
+                }
+            }
+        }
+    }
+    println!();
+    if interactive_element_in_focus {
+        crossterm::execute!(
+            stdout,
+            crossterm::cursor::RestorePosition,
+            crossterm::cursor::Show
+        )
+        .unwrap();
+    } else {
+        crossterm::execute!(stdout, crossterm::cursor::Hide).unwrap();
+    }
 }
 
 enum DisplayBlock {
